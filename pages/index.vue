a15aa3b824f1dd3ca244c4d373bb2ba8d1411f1c
Mathias ROSA <mat.ros@outlook.fr>
2024-04-01 16:08:19 +0200

    responsive

--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,15 @@
                 Se connecter
             </MenuItem>
         </CardHeader>
-        <div class="flex py-3 gap-4">
+        <div class="flex justify-between py-3 gap-4">
             <div class="flex flex-col gap-2">
                 <h2 class="text-accent text-lg">Veuillez vous connecter pour commencer</h2>
-                <p class="text-sm">
+                <p class="text-sm max-w-3xl">
                     Seuls les utilisateurs connectés peuvent créer des liens. Afin de pouvoir utiliser le service, merci de bien vouloir vous connecter à l’aide de votre compte CAS Bordeaux INP par EirbConnect
                 </p>
                 <EirbConnect class="mt-3" />
             </div>
-            <img src="/undraw/undraw_authentication_re_svpt 2.svg" alt="authentification_draw">
+            <img class="sm:flex hidden" src="/undraw/undraw_authentication_re_svpt 2.svg" alt="authentification_draw">
         </div>
     </div>
 </template>
